89b0b9cd21038fe62928411427b82f4dc7f25851
lurichiez-personal <lurichiez@gmail.com>
2025-09-01 18:26:46 -0400

    Add files via upload

--- backend/src/utils/validators.js
+++ backend/src/utils/validators.js
@@ -1,1 +1,12 @@
+function requireFields(body, fields) {
+for (const f of fields) {
+if (body[f] === undefined || body[f] === null || body[f] === "") {
+const err = new Error(`Campo requerido: ${f}`);
+err.status = 400;
+throw err;
+}
+}
+}
 
+
+module.exports = { requireFields };
